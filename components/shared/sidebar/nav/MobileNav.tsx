61a0d044e9bd18f7da6b6103066451843a0ce33b
ishan moorjmalani <ishanmoorjmalani009@gmail.com>
2024-08-28 15:28:17 +0530

    Optimised Local Build for Production Deployment

--- components/shared/sidebar/nav/MobileNav.tsx
+++ components/shared/sidebar/nav/MobileNav.tsx
@@ -1,29 +1,23 @@
 'use client';
-
-import { Badge } from '@/components/ui/badge';
-import { Button } from '@/components/ui/button';
-import { Card } from '@/components/ui/card';
-import { ThemeToggle } from '@/components/ui/theme/theme-toggle';
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from '@/components/ui/tooltip';
 import { UserButton } from '@clerk/nextjs';
+import { Badge } from 'lucide-react';
 import Link from 'next/link';
 import { useConversation } from '../../../../app/hooks/useConversation';
 import { useNavigation } from '../../../../app/hooks/useNavigation';
+import { Button } from '../../../ui/button';
+import { Card } from '../../../ui/card';
+import { Tooltip, TooltipContent, TooltipTrigger } from '../../../ui/tooltip';
 
 const MobileNav = () => {
   const paths = useNavigation();
+
   const { isActive } = useConversation();
 
   if (isActive) return null;
-
   return (
     <Card className="fixed bottom-4 w-[calc(100vw-32px)] flex items-center h-16 p-2 lg:hidden">
       <nav className="w-full">
-        <ul className="flex justify-evenly items-center">
+        <ul className="flex justify-evenly items-center ">
           {paths.map((path, id) => {
             return (
               <li key={id} className="relative">
@@ -36,11 +30,11 @@ const MobileNav = () => {
                       >
                         {path.icon}
                       </Button>
-                      {path.count ? (
-                        <Badge className="absolute left-7 bottom-6">
+                      {path.count !== undefined && path.count > 0 && (
+                        <Badge className="absolute -top-2 -right-2 px-2">
                           {path.count}
                         </Badge>
-                      ) : null}
+                      )}
                     </TooltipTrigger>
                     <TooltipContent>
                       <p>{path.name}</p>
@@ -51,10 +45,9 @@ const MobileNav = () => {
             );
           })}
           <li>
-            <UserButton />
-          </li>
-          <li>
-            <ThemeToggle />
+            <div className="flex flex-col items-center gap-4">
+              <UserButton />
+            </div>
           </li>
         </ul>
       </nav>
